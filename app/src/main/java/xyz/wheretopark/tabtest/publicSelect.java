517411a083f29cbbadd8bd079b350cd63e2294d3
sagar chand agarwal <atomsagar@gmail.com>
2016-01-19 12:19:59 +0530

    App Crashing on Tab

---
+++ app/src/main/java/xyz/wheretopark/tabtest/publicSelect.java
@@ -0,0 +1,13 @@
+package xyz.wheretopark.tabtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class publicSelect extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_public_select);
+    }
+}
