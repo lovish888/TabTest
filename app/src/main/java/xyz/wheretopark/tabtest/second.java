e9cbd6f0e36178f74e4cb876c795995f984a7da5
sagar chand agarwal <atomsagar@gmail.com>
2016-01-19 16:49:13 +0530

    Refresh activity not working on showCompany.class

--- app/src/main/java/xyz/wheretopark/tabtest/second.java
+++ app/src/main/java/xyz/wheretopark/tabtest/second.java
@@ -81,9 +81,7 @@ public class second extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+
 
         return super.onOptionsItemSelected(item);
     }
