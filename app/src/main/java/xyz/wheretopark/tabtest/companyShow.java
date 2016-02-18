8bd1da9e16657cb07c1f16f3e24dd97f1b3b895c
yogeshbalan <balanyogesh@gmail.com>
2016-02-18 21:45:45 +0530

    pull to refresh in corporate activity
    Signed-off-by: yogeshbalan <balanyogesh@gmail.com>

--- app/src/main/java/xyz/wheretopark/tabtest/companyShow.java
+++ app/src/main/java/xyz/wheretopark/tabtest/companyShow.java
@@ -1,11 +1,9 @@
 package xyz.wheretopark.tabtest;
 
 
-
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import java.lang.*;
-
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -19,7 +17,7 @@ import com.parse.ParseQuery;
 import java.util.ArrayList;
 
 
-public class companyShow extends AppCompatActivity {
+public class companyShow extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
 
     static TextView tv;
     static TextView tv1;
@@ -29,24 +27,35 @@ public class companyShow extends AppCompatActivity {
     String[] park_status;
     String delete;
     String d;
+    String myString;
+    private SwipeRefreshLayout swipeRefreshLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_company_show);
         Bundle bundle = null;
         bundle = this.getIntent().getExtras();
-        String myString = bundle.getString("Name");
-        tv=(TextView)findViewById(R.id.company_name);
-        tv1=(TextView)findViewById(R.id.textView4);
+        myString = bundle.getString("Name");
+        tv = (TextView) findViewById(R.id.company_name);
+        tv1 = (TextView) findViewById(R.id.textView4);
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh_layout);
+        swipeRefreshLayout.setOnRefreshListener(this);
+
+        parseDataFromParse();
+
+    }
+
+    public void parseDataFromParse() {
         final ParseQuery<ParseObject> query = ParseQuery.getQuery("Corporate");
         query.whereEqualTo("objectId", myString);
         query.getFirstInBackground(new GetCallback<ParseObject>() {
             public void done(ParseObject object, ParseException e) {
                 if (object != null) {
                     tv.setText(object.getString("company_name"));
-                    d=object.getUpdatedAt().toString().substring(20);
-                    delete=object.getUpdatedAt().toString().replace(d,"");
-                    tv1.setText("Last Updated: "+delete);
+                    d = object.getUpdatedAt().toString().substring(20);
+                    delete = object.getUpdatedAt().toString().replace(d, "");
+                    tv1.setText("Last Updated: " + delete);
                     ArrayList<String> parking_name = (ArrayList<String>) object.get("parking_name");
                     ArrayList<String> park_show_status = (ArrayList<String>) object.get("park_show_status");
                     park_name = parking_name.toArray(new String[parking_name.size()]);
@@ -63,8 +72,13 @@ public class companyShow extends AppCompatActivity {
         ArrayAdapter<String> ad = new ArrayAdapter<String>(this, R.layout.item_layout, park_name);
         lv1.setAdapter(ad);
         lv2 = (ListView) findViewById(R.id.listView2);
-        ArrayAdapter<String> ad2 = new ArrayAdapter<String>(this, R.layout.item_layout,park_status);
+        ArrayAdapter<String> ad2 = new ArrayAdapter<String>(this, R.layout.item_layout, park_status);
         lv2.setAdapter(ad2);
         Toast.makeText(companyShow.this, "You may need to wait as some of the tokens for these slots might be at the exit points.", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onRefresh() {
+        parseDataFromParse();
     }
 }
